e409a24e23efd53e587f7c319ff71864b946d2dc
linkbvian-hash <link.bvian@gmail.com>
2026-09-11 16:59:34 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,1 @@
-const CACHE="mediatheque-pwa-v5";
-const FILES=["./","./index.html","./style.css","./app.js","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{
-  if(new URL(e.request.url).origin===location.origin)
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const CACHE="mediatheque-pwa-v6-logo";const FILES=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/logo-boris-vian.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin!==location.origin)return;if(u.pathname.endsWith("/program.json"))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
