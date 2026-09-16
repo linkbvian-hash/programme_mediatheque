b086f64f05f26c24fcb952b0107c14d3944389d3
linkbvian-hash <link.bvian@gmail.com>
2026-09-16 17:12:46 +0200

    Bumper le cache PWA vers v17

--- service-worker.js
+++ service-worker.js
@@ -12,8 +12,8 @@ firebase.initializeApp({
 
 const messaging=firebase.messaging();
 
-const CACHE="mediatheque-pwa-v16";
-const FILES=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/logo-boris-vian.png","./assets/logo-boris-vian-icon-192.webp?v=16","./assets/logo-boris-vian-icon-96.webp?v=16","./assets/logo-boris-vian-icon-512.png?v=16"];
+const CACHE="mediatheque-pwa-v17";
+const FILES=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/logo-boris-vian.png","./assets/logo-boris-vian-icon-192.png?v=17","./assets/logo-boris-vian-icon-96.webp?v=17","./assets/logo-boris-vian-icon-512.png?v=17"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
